5545300c0435eca24324e511b0c0b1ae663659b0
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-08-11 18:20:03 -0300

    add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,5 +6,23 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#F7F9F9',
+          200: '#DCE9E2',
+          500: '#00856F',
+          700: '#00453A',
+        },
+        beige: '#FFFAF1',
+        dark: {
+          700: '#384633',
+          900: '#061800',
+        },
+      },
+    },
+  },
+  plugins: [],
 }
 export default config
